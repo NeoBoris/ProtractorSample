a11408d55447daa05faab430f7afd19ac3c23a16
kido <neoboris12@gmail.com>
2016-08-02 11:33:31 +0900

    README更新、テストの分割

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# ProtractorSample
+# Protractorを試します
+
+1. Protractorのインストール
+
+    sudo npm install -g protractor  
+    sudo webdriver-manager update  
+    sudo webdriver-manager start  
+
+詳細は[Protractor]で。
+
+[Protractor]: http://www.protractortest.org/#/
+
+2. Protractorの実行
+
+    sudo protractor conf.js
--- spec.js
+++ spec.js
@@ -2,6 +2,9 @@ describe('Protractor Demo App', function() {
   it('should have a title', function() {
     browser.get('http://juliemr.github.io/protractor-demo/');
     expect(browser.getTitle()).toEqual('Super Calculator');
+  });
+  it('3 + 4 = 7', function() {
+    browser.get('http://juliemr.github.io/protractor-demo/');
 
     element(by.model('first')).sendKeys('3');
     element(by.model('second')).sendKeys('4');
